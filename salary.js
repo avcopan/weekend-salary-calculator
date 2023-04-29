4a1f6219a68fe0896474512499205b67d5b7d4d3
Andreas Copan <avcopan@gmail.com>
2023-04-29 14:38:03 -0400

    Implements table row add/remove

--- salary.js
+++ salary.js
@@ -1,5 +1,28 @@
 const submitForm = (event) => {
   event.preventDefault();
-  console.log('submitForm');
-  console.log(event);
+  // Get values from input fields
+  let firstName = document.querySelector('#first-name-input').value;
+  let lastName = document.querySelector('#last-name-input').value;
+  let id = document.querySelector('#id-input').value;
+  let title = document.querySelector('#title-input').value;
+  let salary = document.querySelector('#salary-input').value;
+
+  // Append row to table
+  let salaryTable = document.querySelector('#salary-table-body');
+  salaryTable.innerHTML += `
+    <tr>
+      <td>${firstName}</td>
+      <td>${lastName}</td>
+      <td>${id}</td>
+      <td>${title}</td>
+      <td>${salary}</td>
+      <td>
+        <button onclick="removeEmployee(event)">Delete</button>
+      </td>
+    </tr>
+  `;
 };
+
+const removeEmployee = (event) => {
+  event.target.parentElement.parentElement.remove();
+}
